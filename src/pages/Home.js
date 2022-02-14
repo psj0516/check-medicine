fc7d3bb3e3c6e3b9cf5a2d500c552e045ed015ac
Suji Park <psjhero94@naver.com>
2022-02-14 11:39:47 +0900

    복용 목록 기본 틀 잡기

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,38 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Menu from "../components/Menu";
 
 const Home = () => {
+  const [medication, setMedication] = useState("");
+  const [medications, setMedications] = useState([]);
+
+  const onChange=(event)=> setMedication(event.target.value);
+  const addMedi = (event) => {
+    event.preventDefault();
+    if (medication === ""){
+      return;
+    }
+    setMedications((currentArray) => [medication, ...currentArray]);
+    setMedication("");
+  }
   return (
     <Fragment>
       <Menu />
-      <div>Home</div>
+      <h1>복용 목록 ({medications.length})</h1>
+      <form onSubmit={addMedi}>
+        <input
+          onChange={onChange}
+          type="text"
+          value={medication}
+          placeholder="입력하세요"
+        />
+        <button>추가</button>
+        <hr />
+        <ul>
+          {medications.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </form>
     </Fragment>
   );
 };
