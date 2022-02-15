a45302f319168b6298fb78360e08fe4ea9c7aa48
Suji Park <psjhero94@naver.com>
2022-02-15 17:19:21 +0900

    검색 버튼 클릭 시 검색하도록 변경, 검색 결과에 따라 화면 변화 추가

--- src/pages/Medicine.js
+++ src/pages/Medicine.js
@@ -4,42 +4,63 @@ import Menu from "../components/Menu";
 import Parser from "html-react-parser";
 
 const Medicine = () => {
-    const [loading, setLoading] = useState(true);
-    const [medicines, setMedicines] = useState([]);
-    const [search, setSearch] = useState("");
-    const [datarow, setDatarow] = useState(0);
+  const [medicines, setMedicines] = useState([]);
+  const [search, setSearch] = useState("");
+  const [checkResult, setCheckresult] = useState(true);
+  const [searchResult, setSearchResult] = useState("약 이름을 검색하세요.");
 
-    const getMedicines = async() => {
-        try {
-            const key = process.env.REACT_APP_API_KEY;
-            const response = await axios.get(`/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?ServiceKey=${key}&itemName=${search}&numOfRows=${datarow}&type=json`);
-            const data = response.data.body.items;
-            setMedicines(data);
-        } catch (e) {
-            console.log("에러: " + e)
-        }
+  const onChage = (event) => {
+    setSearch(event.target.value);
+  };
+  const getMedicines = async (event) => {
+    try {
+      event.preventDefault();
+      const key = process.env.REACT_APP_API_KEY;
+      const response = await axios.get(
+        `/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?ServiceKey=${key}&itemName=${search}&numOfRows=10&type=json`
+      );
+      const data = response.data.body.items;
+      setMedicines(data);
+      setSearch("");
+
+      if (!data) {
+        setSearchResult("검색 결과가 없습니다.");
+        setCheckresult(true);
+      } else {
+        setCheckresult(false);
+      }
+    } catch (e) {
+      console.log("에러: " + e);
     }
-    const searchMed=(event)=> {
-        const keyword = event.target.value;
-        setSearch(keyword);
-        setDatarow(10);
-    }
-    useEffect(()=>{
-        getMedicines();
-    }, [search])
+  };
+
   return (
     <Fragment>
       <Menu />
       <div>Medicine</div>
-      <input type="text" placeholder="약 이름을 입력하세요." onChange={searchMed} />
-      {medicines?.map((medicine)=>(
+      <form onSubmit={getMedicines}>
+        <input
+          value={search}
+          type="text"
+          placeholder="약 이름을 입력하세요."
+          onChange={onChage}
+        />
+        <button>검색</button>
+      </form>
+      <div>
+        {checkResult ? (
+          <div>{searchResult}</div>
+        ) : (
           <div>
+            {medicines?.map((medicine) => (
               <ul key={medicine.itemSeq}>
-                  <li>{medicine.itemName}</li>
-                  <li>{Parser(medicine.useMethodQesitm)}</li>
+                <li>{medicine.itemName}</li>
+                <li>{Parser(medicine.useMethodQesitm)}</li>
               </ul>
+            ))}
           </div>
-      ))}
+        )}
+      </div>
     </Fragment>
   );
 };
